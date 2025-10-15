7a3475c09c79c050bc55560be4908614ec81a996
Ivan Panchenko <wao@mail.ru>
2025-10-16 02:10:02 +0300

    update OpenSSL

--- Do.sh
+++ Do.sh
@@ -12,7 +12,7 @@ LUARLRUCACHE="0.15"        # https://github.com/openresty/lua-resty-lrucache/rel
 NDK="0.3.4"                # https://github.com/vision5/ngx_devel_kit/releases
 NGINXPGV="1.0rc7"          # https://github.com/FRiCKLE/ngx_postgres/releases
 SETMISC="0.33"             # https://github.com/openresty/set-misc-nginx-module/releases
-OPENSSL="3.5.2"
+OPENSSL="3.6.0"
 LIBGD="2.3.3"              #https://github.com/libgd/libgd/releases
 
 
