157b51b21a0916461d921216a882d46727548b74
Ivan Panchenko <wao@mail.ru>
2026-02-06 14:11:25 +0300

    upgrade nginx version

--- Do.sh
+++ Do.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e -u
 
-NGV="1.28.1"               # 
+NGV="1.28.2"               # 
 
 ECHO="0.63"                # https://github.com/openresty/echo-nginx-module/releases
 LUAMOD="0.10.28"           # https://github.com/openresty/lua-nginx-module/releases
